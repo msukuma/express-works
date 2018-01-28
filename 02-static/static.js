9ed49bcd7bf250103c0449ce3a57c1010e7cf3c2
Itenga <itenga@protonmail.com>
2018-01-28 14:42:13 +0300

    02-static

---
+++ 02-static/static.js
@@ -0,0 +1,7 @@
+const { createReadStream } = require('fs');
+const express = require('express');
+const app = express();
+
+app.get('/', express.static(process.argv[3]));
+
+app.listen(process.argv[2]);
